dc00c8df9e54d6605eb0d299a61b37e2a0c9c994
Bermet Bakytbekova <bembakyt@gmail.com>
2021-12-17 17:52:02 +0600

    hw 17-18 finished

--- app/src/main/java/com/ber/android_hws/ProfileFragment.kt
+++ app/src/main/java/com/ber/android_hws/ProfileFragment.kt
@@ -12,14 +12,11 @@ class ProfileFragment: Fragment(R.layout.profile_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val toolbar = view.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_profile)
-        toolbar.inflateMenu(R.menu.profile_menu)
+        toolbar.inflateMenu(R.menu.menu)
 
         toolbar.setNavigationIcon(androidx.appcompat.R.drawable.abc_ic_ab_back_material)
         toolbar.setNavigationOnClickListener {
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, MainFragment())
-                .addToBackStack(null)
-                .commit()
+            requireActivity().onBackPressed()
         }
     }
 }
